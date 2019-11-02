29be0bd1f4d00f7686553856d379a9ff446e782e
Sam Gildea <sam.c.gildea@gmail.com>
2019-11-02 11:47:11 -0400

    small search changes

--- src/Components/SearchItem.js
+++ src/Components/SearchItem.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 const SearchItem = ({ item }) => {
   return (
     <div className="search-item">
-      <a href={item.url}>{item.title}</a>
+      <a href="#">{item.name}</a>
     </div>
   );
 };
