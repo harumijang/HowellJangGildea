51fb63b13976259cf03e06aed7b2a88356267aef
Sam Gildea <sam.c.gildea@gmail.com>
2019-11-07 14:13:48 -0500

    fixing route

--- src/Components/GamePageBody.jsx
+++ src/Components/GamePageBody.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from "react";
 
-const GamePageBody = ({game}) => {
-    return (
-        <div>
-    <div class="d-flex">
-       <img src="https://picsum.photos/id/119/200/300"></img>
-       <div class="d-flex flex-column">
-       <h1>Game Title</h1>
-           <div class="d-flex"><h1>Score 8.3/10</h1> <h2>Rate</h2></div>
-           <p>ADD MORE STUFF HERE LATER</p>
-            </div>
+const GamePageBody = ({ game }) => {
+  return (
+    <div>
+      <div class="d-flex">
+        <img src="https://picsum.photos/id/119/200/300"></img>
+        <div class="d-flex flex-column">
+          <h1>Game Title</h1>
+          <div class="d-flex">
+            <h1>Score 8.3/10</h1> <h2>Rate</h2>
+          </div>
+          <p>ADD MORE STUFF HERE LATER</p>
         </div>
-        <h3>Lead Dev:</h3>
-        <h3>Studio:</h3>
-        <h3>Publisher</h3>
-        </div>
-    )
-}
-export default GamePageBody
+      </div>
+      <h3>Lead Dev:</h3>
+      <h3>Studio:</h3>
+      <h3>Publisher</h3>
+    </div>
+  );
+};
+export default GamePageBody;
--- src/Components/SearchItem.js
+++ src/Components/SearchItem.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Route, Router, Link } from "react-router-dom";
 import GamePrototype from "../Containers/GamePrototype";
+import GamePage from "../components/GamePage";
+
 
 
 const SearchItem = ({ game }) => {
@@ -9,7 +11,7 @@ const SearchItem = ({ game }) => {
       <div className="search-item">
         <Route
           path="game/:game.name"
-          render={() => <GamePrototype game={game} />}
+          render={() => <GamePage game={game} />}
         >
           {game.name}
         </Route>
