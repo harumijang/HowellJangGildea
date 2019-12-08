fece9b33c4f67dbe3e945cb2279334f25f99f73d
harumijang <h.jang521@gmail.com>
2019-12-08 13:19:18 -0500

    made graphic nonclickable

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -7,7 +7,7 @@ import banner from "../Content/banner.png";
 const Banner = () => {
     return (
         // <div class="d-flex justify-content-between" id="banner">
-<div style={{display: 'flex', justifyContent: 'center'}}>
+<div id= "banner-pic" style={{display: 'flex', justifyContent: 'center'}}>
      <Link to="/"><img src={banner} 
     //  height="92" width="400"
      /></Link>
